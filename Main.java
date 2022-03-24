a5ddb660b8de85d541b4bda7bf512aae3ebfd9ae
радон <96480522+RadonRebirth@users.noreply.github.com>
2022-03-24 19:20:20 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,138 +1,58 @@
 package com.company;
+
+import java.util.Scanner;
+
+class Animal {
+    String name;
+    String food;
+    String location;
+    public Animal(String name, String food, String location) {
+        this.name = name;
+        this.food = food;
+        this.location = location;
+    }
+}
+class Cat extends Animal {
+    public Cat(String name, String food, String location) {
+        super(name, food, location);
+    }
+}
+class Dog extends Animal {
+    public Dog(String name, String food, String location)
+    {
+        super(name, food, location);
+    }
+}
+class Horse extends Animal {
+    public Horse(String name, String food, String location) {
+        super(name, food, location);
+    }
+}
+class Veterinar{
+    public void treatAnimal(Animal animal){
+        System.out.println("\nИнформация о пациенте:");
+        System.out.println("на приёме у ветеринара сейчас " + animal.name);
+        System.out.println("Имя питомца: " + animal.name +  "\nПредпочитаемое питание: " + animal.food  + "\nПоследнее место нахождение: " + animal.location);
+        System.out.print("");
+    }
+}
 public class Main {
-
-    public static class Animal {
-        Animal animal;
-        public String name;
-        public String food;
-        public String location;
-
-        public String setFood() {
-            return food;
-        }
-
-        Animal(String name, String food, String location) {
-            this.name = name;
-            this.food = food;
-            this.location = location;
-        }
-        public void eat() {
-            System.out.println("жует \n кушает");
-        }
-
-        public void makeNoise() {
-            System.out.println("шумит");
-        }
-
-        public void sleep() {
-            System.out.println("спит");
-        }
-
-        public void displayInfo() {
-            System.out.println("расположение: " + location);
-            System.out.println("еда: " + food);
-        }
-    }
-
-    static class Hourse extends Animal {
-        private String minus = "Непосллушный";
-
-        Hourse(String name, String food, String location) {
-            super(name, food, location);
-        }
-
-        public void eat() {
-            System.out.println("Конь кушает" + setFood());
-        }
-
-        public void makeNoise() {
-            System.out.println("Конь ржёт");
-        }
-
-        public void sleep() {
-            System.out.println("Конь спит");
-        }
-
-        public void displayInfo() {
-            eat();
-            makeNoise();
-            sleep();
-            System.out.println("минус: " + minus);
-        }
-    }
-
-    static class Cat extends Animal {
-        private String minus = "Слишком ленивый";
-
-        Cat(String name, String food, String location) {
-            super(name, food, location);
-        }
-
-        public void eat() {
-            System.out.println("Кот кушает" + setFood());
-        }
-
-        public void makeNoise() {
-            System.out.println("Кот мурлычыт");
-        }
-
-        public void sleep() {
-            System.out.println("Кот спит");
-        }
-
-        public void displayInfo() {
-            eat();
-            makeNoise();
-            sleep();
-            System.out.println("минус: " + minus);
-        }
-    }
-
-    static class Dog extends Animal {
-        private String minus = "Слишком громкий";
-
-        Dog(String name, String food, String location) {
-            super(name, food, location);
-        }
-
-        public void eat() {
-            System.out.println("Собака кушает" + setFood());
-        }
-
-        public void makeNoise() {
-            System.out.println("Собака лает");
-        }
-
-        public void sleep() {
-            System.out.println("Собачка спит");
-        }
-
-        public void displayInfo() {
-            eat();
-            makeNoise();
-            sleep();
-            System.out.println("минус: " + minus);
-        }
-    }
-
-    static class Veterinar {
-        void treatAnimal(Animal animal) {
-            System.out.print("\n еда: " + animal.food);
-            System.out.println("\n расположение: " + animal.location);
-            System.out.println(animal.name + " у Ветеринара");
-        }
-    }
-
     public static void main(String[] args) {
         Veterinar vet = new Veterinar();
-        Animal cat = new Cat("Барсик", " китикет", "Дома на диване");
-        Animal dog = new Dog("Рекс", " собачий корм", "Будка");
-        Animal hourse = new Hourse("Искорка", " яблоко", "Конюшня");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Как зовут котёнка: ");
+        String name = in.nextLine();
+        Cat cat = new Cat(name,"Кошачий корм","Дома на кроватке");
         vet.treatAnimal(cat);
-        cat.displayInfo();
+        System.out.println(" ");
+        System.out.println("Как зовут собачку: ");
+        name = in.nextLine();
+        Dog dog = new Dog(name, "Собачий корм","Во дворе в будке");
         vet.treatAnimal(dog);
-        dog.displayInfo();
-        vet.treatAnimal(hourse);
-        hourse.displayInfo();
+        System.out.println(" ");
+        System.out.println("Как зовут лошадь: ");
+        name = in.nextLine();
+        Horse horse = new Horse(name,"Яблоко","В загоне на конюшне");
+        vet.treatAnimal(horse);
     }
 }
